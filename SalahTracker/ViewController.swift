d6635a2357ee301ffc27f35f13833b384eb47431
wahab202 <abdul.wahabaziz20@gmail.com>
2020-07-22 17:21:48 +0500

    first commit

---
+++ SalahTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SalahTracker
+//
+//  Created by Abdul Wahab on 7/22/20.
+//  Copyright © 2020 Abdul Wahab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
